58b9d4901f8533cff9b1cd58c87f66a157c29109
Simo Sipila <sipilasimo@gmail.com>
2024-02-01 14:22:51 +0200

    puhelinluettelo

--- osa2/puhelinluettelo/src/App.jsx
+++ osa2/puhelinluettelo/src/App.jsx
@@ -1,24 +1,40 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import PersonForm from './components/PersonForm'
 import Filter from './components/Filter'
 import Persons from './components/Persons'
+import numberService from './services/numbers'
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ])
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [newFilter, setNewFilter] = useState('')
 
+  useEffect(() => {
+    numberService
+      .getAll()
+        .then(initialNumbers => {
+        setPersons(initialNumbers)
+      })
+  }, [])
+
+  
   const handleNumberChange = (event) => setNewNumber(event.target.value)
 
   const handleNameGhange = (event) => setNewName(event.target.value)
 
   const handleFilterChange = (event) => setNewFilter(event.target.value)
+
+  const handleDeleteClick = (id) => {
+    const name = persons.find(person => person.id = id).name
+    window.confirm(`delete ${name}?`)
+    ? numberService
+        .deleteNumber(id)
+          .then(() => {
+            setPersons(persons.filter(person => person.id !== id))
+          })
+    : null
+  }
 
   const addPerson = (event) => {
     event.preventDefault()
@@ -26,14 +42,18 @@ const App = () => {
       name: newName,
       number: newNumber
     }
-
     const names = persons.map(person => person.name)
-    !names.includes(newName) ? 
-    setPersons(persons.concat(person)) 
-    : alert(`${newName} is already added to phonebook`)
 
-    setNewName('')
-    setNewNumber('')
+    !names.includes(newName) 
+    ? numberService
+        .create(person)
+          .then(returnedPerson => {
+          console.log(returnedPerson)
+          setPersons(persons.concat(returnedPerson))
+          setNewName('')
+          setNewNumber('')
+          })
+    : alert(`${newName} is already added to phonebook`)
   }
 
   return (
@@ -44,7 +64,7 @@ const App = () => {
       <PersonForm addPerson={addPerson} newName={newName} handleNameGhange={handleNameGhange} 
         newNumber={newNumber} handleNumberChange={handleNumberChange}/>
       <h2>Numbers</h2>
-      <Persons persons={persons} filter={newFilter}/>
+      <Persons persons={persons} filter={newFilter} handleClick={handleDeleteClick}/>
     </div>
   )
 
--- osa2/puhelinluettelo/src/components/Persons.jsx
+++ osa2/puhelinluettelo/src/components/Persons.jsx
@@ -1,9 +1,14 @@
 
-const Persons = ({ persons, filter }) => {
+const Persons = ({ persons, filter, handleClick}) => {
 	const personsToShow = persons.filter(person => person.name.toLowerCase().includes(filter.toLowerCase()))
 	return (
 		personsToShow.map(person => 
-			<p key={person.name}>{person.name} {person.number}</p>)
+			<div key={person.name}>
+				{person.name} {person.number}
+				<button onClick={() => handleClick(person.id)}>
+					delete
+				</button>
+			</div>)
 	)
 }
 
